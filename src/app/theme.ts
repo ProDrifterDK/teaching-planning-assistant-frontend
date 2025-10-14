83e8a749a6e726b9a25a1ce7efd5bd1d20df87bd
ProDrifterDK <alan.resyst@gmail.com>
2025-10-14 18:04:01 -0300

    feat: Implement MUI theme engine

---
+++ src/app/theme.ts
@@ -0,0 +1,32 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  palette: {
+    mode: 'light', // or 'dark'
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#red',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+export default theme;
